5d754faa0b991c195fb8cc6f9eebb14bf6e7856d
joe <joe@localhost>
2010-06-27 20:59:07 -0500

    prototypes

--- src/test/java/nofs/restfs/tests/RestFsIntegrationTests.java
+++ src/test/java/nofs/restfs/tests/RestFsIntegrationTests.java
@@ -1,7 +1,25 @@
 package nofs.restfs.tests;
 
-import nofs.restfs.tests.util.RestExampleRunner;
+import java.util.Collection;
+import java.util.Iterator;
 
+import nofs.restfs.tests.util.RestExampleRunner;
+import nofs.restfs.tests.util.TestClient;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpConnectionManager;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
+import org.apache.jackrabbit.webdav.DavConstants;
+import org.apache.jackrabbit.webdav.MultiStatus;
+import org.apache.jackrabbit.webdav.client.methods.DavMethod;
+import org.apache.jackrabbit.webdav.client.methods.PropFindMethod;
+import org.apache.jackrabbit.webdav.property.DavPropertySet;
+import org.apache.jackrabbit.webdav.property.DefaultDavProperty;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,8 +38,70 @@ public class RestFsIntegrationTests {
 		_restRunner.StopRunner();
 	}
 	
+	private static void TestHttpMethod(String line) throws Exception {
+		//final String host = "joekaylor.net";
+		//final int port = 80;
+		final String host = "127.0.0.1";
+		final int port = 8100;
+		TestClient client = new TestClient();
+		WriteResponse(client.WriteHTTP(host, port, line));
+	}
+	
+	private static void WriteResponse(String response) {
+		System.out.println("***************");
+		System.out.println(response);
+		System.out.println("***************");
+	}
+	
+	@Test
+	public void TestOptionsMethodBehavior() throws Exception {
+		TestHttpMethod("OPTIONS /users/1 HTTP/1.1");
+	}
+	
+	@Test
+	public void TestGetHttpMethodBehavior() throws Exception {
+		TestHttpMethod("GET /users/1 HTTP/1.1");
+	}
+	
+	@Test
+	public void TestPropDefMethodBehavior() throws Exception {
+		TestHttpMethod("PROPDEF /users/1 HTTP/1.1");
+	}
+	
+	@SuppressWarnings("unchecked")
 	@Test
 	public void Test1() throws Exception {
-		Thread.sleep(15000);
+		final String host = "127.0.0.1:8100";
+		final String path = "/users/1";
+		
+		HostConfiguration hostConfig = new HostConfiguration();
+        hostConfig.setHost(host); 
+        HttpConnectionManager connectionManager = new MultiThreadedHttpConnectionManager();
+        HttpConnectionManagerParams params = new HttpConnectionManagerParams();
+        int maxHostConnections = 20;
+        params.setMaxConnectionsPerHost(hostConfig, maxHostConnections);
+        connectionManager.setParams(params);    
+        HttpClient client = new HttpClient(connectionManager);
+        Credentials creds = new UsernamePasswordCredentials("userId", "pw");
+        client.getState().setCredentials(AuthScope.ANY, creds);
+        client.setHostConfiguration(hostConfig);
+        
+        DavMethod pFind = new PropFindMethod(
+        		"http://" + host + path, 
+        		DavConstants.PROPFIND_ALL_PROP, DavConstants.DEPTH_INFINITY);
+        client.executeMethod(pFind);
+
+        MultiStatus multiStatus = pFind.getResponseBodyAsMultiStatus();      
+             
+        //Not quite nice, but for a example ok
+        DavPropertySet props = multiStatus.getResponses()[0].getProperties(200);
+              
+        Collection<DefaultDavProperty> propertyColl= (Collection<DefaultDavProperty>)props.getContent(); 
+        propertyColl.iterator();
+        for(Iterator<DefaultDavProperty> iterator = propertyColl.iterator(); iterator.hasNext();){
+            DefaultDavProperty tmpProp=iterator.next();
+            System.out.println(tmpProp.getName() +"  "+ tmpProp.getValue());
+        }
+
 	}
 }
---
+++ src/test/java/nofs/restfs/tests/util/TestClient.java
@@ -0,0 +1,69 @@
+package nofs.restfs.tests.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.Socket;
+
+public class TestClient {
+	
+	public String WriteHTTP(String host, int port, String req) throws Exception {
+    	final String nl = "\r\n";
+        final String data = req + nl + "Host: " + host + nl + nl;
+        Socket sock = new Socket(host, port);
+        OutputStream out = sock.getOutputStream();
+        try {
+        	WriteData(out, data);
+        	InputStream in = sock.getInputStream();
+        	DataReader reader = new DataReader(in);
+        	reader.start();
+        	reader.join(2500);
+        	return reader.getData();
+        } finally {
+        	sock.close();
+        }        
+    }
+    
+    private static void WriteData(OutputStream out, String data) throws Exception {
+    	for(byte value : data.getBytes()) {
+    		out.write(value);
+    	}
+    	out.flush();
+    }
+    
+    private class DataReader extends Thread {
+    	private final InputStream _in;
+    	private final StringBuilder _buffer;
+    	
+    	public DataReader(InputStream in) {
+    		_in = in;
+    		_buffer = new StringBuilder();
+    		super.setDaemon(true);
+    	}
+    	
+    	public String getData() {
+    		synchronized(_buffer) {
+    			return _buffer.toString();
+    		}
+    	}
+    	
+    	@Override
+    	public void run() {
+    		try {
+	    		Reader reader = new InputStreamReader(_in);
+	        	LineNumberReader lineReader = new LineNumberReader(reader);
+	        	String line;
+	    		while((line = lineReader.readLine()) != null) {
+	        		synchronized(_buffer) {
+	        			_buffer.append(line);
+	        			_buffer.append("\r\n");
+	        		}
+	        	}
+    		} catch (IOException ioe) {    			
+    		}
+    	}
+    }
+}
