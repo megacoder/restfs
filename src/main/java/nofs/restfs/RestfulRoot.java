03377a56c21815d2938f1d818b8c58204377f65d
joe <joe@localhost>
2010-07-11 17:44:06 -0500

    fixed root object to be domain object

--- src/main/java/nofs/restfs/RestfulRoot.java
+++ src/main/java/nofs/restfs/RestfulRoot.java
@@ -1,9 +1,13 @@
 package nofs.restfs;
 
+import nofs.Library.Annotations.DomainObject;
+import nofs.Library.Annotations.FolderObject;
 import nofs.Library.Annotations.RootFolderObject;
 
 @SuppressWarnings("serial")
 @RootFolderObject
+@FolderObject
+@DomainObject
 public class RestfulRoot extends RestfulFolder {
 
 }
